fa4feb6a1a32a555902453bcfa2572564d32d70b
unknown <fmmantelli@gmail.com>
2022-04-12 20:21:25 -0300

    added project from localhost

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,72 @@
+import { useTransactions } from '../../hooks/useTransactions';
+
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+    const { transactions } = useTransactions();
+
+    const summary = transactions.reduce((acc, transaction) => {
+        switch (transaction.type) {
+            case 'deposit':
+                acc.deposits += transaction.amount;
+                acc.total += transaction.amount;
+                break;
+            case 'withdraw':
+                acc.withdrawals += transaction.amount;
+                acc.total += transaction.amount;
+                break;
+            default:
+                break;
+        }
+        return acc;
+    }, {
+        deposits: 0,
+        withdrawals: 0,
+        total: 0
+    })
+
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.deposits)}
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Saidas" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.withdrawals)}
+                </strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>
+                    {new Intl.NumberFormat('pt-BR', {
+                        style: 'currency',
+                        currency: 'BRL'
+                    }).format(summary.total)}
+                </strong>
+            </div>
+        </Container>
+    )
+}
